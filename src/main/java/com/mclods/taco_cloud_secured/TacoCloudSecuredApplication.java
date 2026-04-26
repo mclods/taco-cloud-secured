c83b0d647db4fc7857d57e4d36a10570a47d3dc3
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-04-26 11:57:12 +0530

    First Commit

---
+++ src/main/java/com/mclods/taco_cloud_secured/TacoCloudSecuredApplication.java
@@ -0,0 +1,13 @@
+package com.mclods.taco_cloud_secured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudSecuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudSecuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mclods/taco_cloud_secured/TacoCloudSecuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mclods.taco_cloud_secured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudSecuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
