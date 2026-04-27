fdce5bd6ee59c716402e1137f16e4583cd189b55
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-04-27 21:43:06 +0530

    Added In Memory UserDetailsService Bean (#4)

---
+++ src/main/java/com/mclods/taco_cloud_secured/configuration/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.mclods.taco_cloud_secured.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(new User(
+                "buzz",
+                passwordEncoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        ));
+        userDetailsList.add(new User(
+                "woody",
+                passwordEncoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        ));
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+}
